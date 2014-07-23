ae5e03b3187dc4053358fbb3a042423576be6ec1
Scott Corbett <secorbett13@gmail.com>
2014-07-23 07:16:17 -0400

    Added solution folders for Comprehensive and final projects Added Assignment2B project

---
+++ Assignment1A/Assignment2B/Main.cpp
@@ -0,0 +1,25 @@
+// Name:					Scott Corbett
+// College ID:				0561772
+// Telephone:				848-391-6953
+// Email:					secorbett13@gmail.com
+// College and Semester:	TESC, July 2014
+// Course Code:				COS-213-GS007
+// Course Name:				C++ Programming
+//
+// Assignment 2B
+//	Design and run a program that takes a numerical score and outputs a
+//	letter grade.Specific numerical scoresand letter grades are listed below :
+//	90–100 = Grade A
+//	80–89 = Grade B
+//	70–79 = Grade C
+//	60–69 = Grade D
+//	0–59 = Grade F
+
+#include <array>
+#include <iomanip>
+#include <iostream>
+
+int main()
+{
+	return 0;
+}
